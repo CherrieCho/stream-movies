19bc0dbe5f74cdf2564a0b27da4b95f935dd6426
cherrie <cherry199848@gmail.com>
2025-04-23 23:37:47 +0900

    modified pagination style

--- src/pages/Movies/MoviePage.jsx
+++ src/pages/Movies/MoviePage.jsx
@@ -6,8 +6,9 @@ import MovieCard from '../../common/movieCard/MovieCard';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import ReactPaginate from 'react-paginate';
 import "./MoviePage.style.css";
+import Pagination from '@mui/material/Pagination';
+import Stack from '@mui/material/Stack';
 
 //1. movies 카테고리 클릭해서 진입(popular영화 보여주기)
 //2. 키워드 검색해서 진입
@@ -16,18 +17,30 @@ const MoviePage = () => {
   const [query, setQuery] = useSearchParams();
   //url에서 쿼리값 읽어와서 키워드로 설정하고 서치훅에 넘겨주기
   const keyword = query.get("q");
-  console.log(keyword);
   const {data, isLoading, isError, error} = useSearchMovieQuery({keyword, page});
 
 
-  const handlePageClick = ({selected}) => {
-    setPage(selected + 1);  //선택한 페이지로 바꿔주기(3을 선택하면 2로 인식해서 +1 해줘야함)
-  }
+  const handlePageChange = (event, value) => {
+    setPage(value);
+  };
+
+  const getSize = () => window.innerWidth < 576 ? "small" : "medium";
+  const [paginationSize, setPaginationSize] = useState(getSize());
 
   //keyword가 바뀌면 페이지 초기화
   useEffect(() => {
     setPage(1);
   }, [keyword]);
+
+  //페이지네이션 크기조절
+  useEffect(() => {
+    const handleResize = () => {
+      setPaginationSize(getSize());
+    };
+
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
 
   if(isLoading){
     return <h1>Loading...</h1>
@@ -48,34 +61,29 @@ const MoviePage = () => {
       <Container>
         <Row>
           {data?.results.map((movie, index) => (
-            <Col xl={3} lg={4} md={6} key={index}>
+            <Col xl={3} md={6} key={index} className='movie-result'>
               <MovieCard movie={movie} key={index}/>
             </Col>
           ))}
         </Row>
         <Row>
-          <Col>
-          <ReactPaginate
-        nextLabel=">"
-        onPageChange={handlePageClick}
-        pageRangeDisplayed={2}
-        marginPagesDisplayed={2}
-        pageCount={data?.total_pages > 500 ? 500 : data.total_pages}  //전체페이지
-        forcePage={page-1}  //현재페이지(0부터 카운트함)
-        previousLabel="<"
-        pageClassName="page-item"
-        pageLinkClassName="page-link"
-        previousClassName="page-item"
-        previousLinkClassName="page-link"
-        nextClassName="page-item"
-        nextLinkClassName="page-link"
-        breakLabel="..."
-        breakClassName="page-item"
-        breakLinkClassName="page-link"
-        containerClassName="pagination"
-        activeClassName="active"
-        renderOnZeroPageCount={null}
-      />
+          <Col className="d-flex justify-content-center mt-4">
+          <Stack spacing={2}>
+              <Pagination
+                count={data?.total_pages > 500 ? 500 : data.total_pages}
+                page={page}
+                onChange={handlePageChange}
+                size={paginationSize}
+                color="primary"
+                sx={{
+                  '& .MuiPaginationItem-root': {
+                    color: 'white', // 텍스트 색상 변경
+                  }
+                }}
+                showFirstButton
+                showLastButton
+              />
+            </Stack>
           </Col>
         </Row>
       </Container>
